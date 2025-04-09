6185da75effb1af1be59685b33cf82c09f043395
Matheus Guida <159961710+Nevek20@users.noreply.github.com>
2025-04-09 10:00:26 -0300

    Update GerCupom.cs

--- dream game/GerCupom.cs
+++ dream game/GerCupom.cs
@@ -33,7 +33,7 @@ namespace dream_game
 
         private void CarregarCupons(string filtro = "")
         {
-            string conexaoString = "Server=localhost; Port=3306; Database=bd_gamexchange; Uid=root; Pwd=;";
+            string conexaoString = "Server=localhost; Port=3306; Database=u531683190_bd_gamexchange; Uid=u531683190_ryan; Pwd=RyanGuida123;";
             string query = "SELECT id_cupom, nome_cupom, desconto FROM tb_cupom";
 
             if (!string.IsNullOrWhiteSpace(filtro))
